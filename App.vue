804a06cc28d9b1389794d3f58e48752869060d22
creandotec <ing.amepa@gmail.com>
2021-02-06 14:24:51 -0600

    Inicio del proyecto Innova

---
+++ App.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="container">
+    <div class="innovaBg"></div>
+    <text>{{message}}</text>
+  </view>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Hello World"
+    };
+  }
+};
+</script>
+
+<style>
+.innovaBg {
+  background-image:url(https://vuejs.org/images/logo.png);
+}
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
