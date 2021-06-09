0e55cc92f301cf4fdd1a51b42a942657b22dbddd
creandotec <ing.amepa@gmail.com>
2021-06-08 19:23:36 -0500

    websocket test

--- components/InnovaHeader.vue
+++ components/InnovaHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <view class="header">
-        
+        <ws/>
         <view style="flex:0.35; flex-direction:row; justify-content:center; align-items:stretch;">
             <Pressable :on-press='() => goHome()'>
                 <image class="home-button"
@@ -10,14 +10,25 @@
         </view>
         
         <view style="flex:1; flex-direction:column;">
-            <image class="header-bar" :source="require('./../assets/Innova/BG/banerhomesolo.png')"
-                resizeMode="cover"/>
+            <!-- <image class="header-bar" :source="require('./../assets/Innova/BG/banerhomesolo.png')"
+                resizeMode="cover"/> -->
+            <ImageBackground class="header-bar" 
+                :source="require('./../assets/Innova/BG/banerhomesolo.png')">
+                <Text style="margin-left:auto; margin-right:5%; margin-top:5%;">30.0°F</Text>
+            </ImageBackground>
+            
         </view>
     </view>    
 </template>
 
 <script>
+
+import WS from 'react-native-websocket';
+
 export default {
+    components:{
+        "ws":WS
+    },
     props:{
         navigation:{
             type: Object
